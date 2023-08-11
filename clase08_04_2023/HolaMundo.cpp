286c9b7d2afd6d7b78b87a9f0a304fae6ce25ef1
Jose Duarte <josepipe0909@gmail.com>
2023-08-11 14:42:39 -0500

    comentarios en el programa

--- clase08_04_2023/HolaMundo.cpp
+++ clase08_04_2023/HolaMundo.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-main()
+#include <iostream> // para cout
+#include <stdlib.h> // para system("pause")
+using namespace std; // para no escribir std::cout
+main() // funcion principal
 {
-    cout << "Hola Mundo" << endl;
-    system("pause");
+    cout << "Hola Mundo" << endl; // imprime Hola Mundo, cout es un objeto de la clase ostream, endl es un manipulador de flujo
+    system("pause"); // pausa el programa
 }
