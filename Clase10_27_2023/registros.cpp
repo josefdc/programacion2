170b94dcdedd10a4862fced95319f577d10479aa
Jose Felipe Duarte <jose.duarte@utp.edu.co>
2023-10-28 16:29:36 -0500

    mejorar errores de struct

--- Clase10_27_2023/registros.cpp
+++ Clase10_27_2023/registros.cpp
@@ -1,68 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
+#include <stdbool.h>
 
-struct Estudiantes
-{
+struct Estudiantes {
     char Cod[6];
-    char Nombre[20];
-    float Nota1, Nota2, Nota3;
+    char Nombre[50]; // Aumentado para aceptar nombres más largos
+    float Nota1, Nota2, Nota3, Promedio;
 };
-void adiccionar(Estudiantes Est[], int n);
-void buscar (char Codigo[], Estudiantes E [], int n);
 
-int main()
-{
-    
+void adicionar(Estudiantes Est[], int n);
+void buscar(char Codigo[], Estudiantes E[], int n);
+
+int main() {
     int n;
     char Cod[6];
-    printf("Cantidad de estudiantes: ");
-    scanf("%d", &n); 
-    Estudiantes Est[n];
     setlocale(LC_ALL, "spanish");
-    adiccionar(Est, n);
+    printf("Cantidad de estudiantes: ");
+    scanf("%d", &n);
+    getchar(); // Consumir el '\n' que queda en el búfer
+    Estudiantes Est[n];
+    adicionar(Est, n);
     printf("Ingrese el codigo del estudiante a buscar: ");
-    scanf("%s", Cod);
+    fgets(Cod, sizeof(Cod), stdin);
+    Cod[strcspn(Cod, "\n")] = 0; // Eliminar el '\n' al final
     buscar(Cod, Est, n);
-    
 }
 
-void adiccionar(Estudiantes Est[], int n){
-    float promedio;
-    for (int i = 0; i < n; i++)
-    {
+void adicionar(Estudiantes Est[], int n) {
+    for (int i = 0; i < n; i++) {
         printf("Ingrese el codigo del estudiante: ");
-        scanf("%s", Est[i].Cod); 
+        fgets(Est[i].Cod, sizeof(Est[i].Cod), stdin);
+        Est[i].Cod[strcspn(Est[i].Cod, "\n")] = 0; // Eliminar el '\n' al final
         printf("Ingrese el nombre del estudiante: ");
-        scanf("%s", Est[i].Nombre); 
+        fgets(Est[i].Nombre, sizeof(Est[i].Nombre), stdin);
+        Est[i].Nombre[strcspn(Est[i].Nombre, "\n")] = 0; // Eliminar el '\n' al final
         printf("Ingrese la nota 1: ");
         scanf("%f", &Est[i].Nota1);
         printf("Ingrese la nota 2: ");
         scanf("%f", &Est[i].Nota2);
         printf("Ingrese la nota 3: ");
         scanf("%f", &Est[i].Nota3);
-        promedio=(Est[i].Nota1+Est[i].Nota2+Est[i].Nota3)/3;
-        printf("El promedio es: %.2f\n----------------\n", promedio);
+        getchar(); // Consumir el '\n' que queda en el búfer
+        Est[i].Promedio = (Est[i].Nota1 + Est[i].Nota2 + Est[i].Nota3) / 3;
+        printf("El promedio es: %.2f\n----------------\n", Est[i].Promedio);
     }
 }
 
-void buscar (char Codigo[], Estudiantes E [], int n)
-{
-    float promedio;
-    for (int i = 0; i < n; i++)
-    {
-        if (strcmp(Codigo, E[i].Cod)==0)
-        {
+void buscar(char Codigo[], Estudiantes E[], int n) {
+    bool encontrado = false;
+    for (int i = 0; i < n; i++) {
+        if (strcmp(Codigo, E[i].Cod) == 0) {
+            encontrado = true;
             printf("datos del estudiante\n");
             printf("Codigo: %s\n", E[i].Cod);
             printf("Nombre: %s\n", E[i].Nombre);
             printf("Nota 1: %.2f\n", E[i].Nota1);
             printf("Nota 2: %.2f\n", E[i].Nota2);
             printf("Nota 3: %.2f\n", E[i].Nota3);
-            promedio=(E[i].Nota1+E[i].Nota2+E[i].Nota3)/3;
-            printf("El promedio es: %.2f\n----------------\n", promedio);
+            printf("El promedio es: %.2f\n----------------\n", E[i].Promedio);
             break;
         }
+    }
+    if (!encontrado) {
         printf("El codigo no existe\n");
     }
 }
